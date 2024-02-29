f0d7028ec05c32d3bbadbcae6e679cf5bcea7d9e
amolinari94 <92828306+amolinari94@users.noreply.github.com>
2024-02-29 15:49:46 -0600

    made branch

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fac4eeb400b8719524544ba6d4600c7e3350e94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a2fe27d7cce4386fb813017764f757855bc52c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
