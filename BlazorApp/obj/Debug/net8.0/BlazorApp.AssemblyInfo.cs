116b3ce20ba91bb5e721c2b69b3fb867935ffc31
amolinari94 <92828306+amolinari94@users.noreply.github.com>
2024-02-29 14:24:02 -0600

    adding .vscode folder

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dcf4ae486d69dc82d7331606fca062cae75c82d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc92cec91f85a0fde1fb7ba607bdb4af580c05be")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
