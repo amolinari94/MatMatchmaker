6b80e2dade3666dfce8df29e818656eca1c34154
Clayton5456 <71726374+Clayton5456@users.noreply.github.com>
2024-03-19 13:47:21 -0500

    Comitting?

--- BlazorApp/obj/Release/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Release/net8.0/BlazorApp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a669f3ff655a99d0e53cca63ccaaa22b8c5f942c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
