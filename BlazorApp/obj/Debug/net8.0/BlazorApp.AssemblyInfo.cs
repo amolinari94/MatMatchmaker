f9f1f0135d93904dc1dccebd9a26110514fc27ce
amolinari94 <92828306+amolinari94@users.noreply.github.com>
2024-04-10 16:24:23 -0500

    Added BlazorServer App package references and calls in BlazorApp.csproj, Program.cs

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aebae68ebd60ec38597621c217f90289d28a69e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b85483a9b16d977fa0afcd3e246ac0dd06f339d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
