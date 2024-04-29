65b3e229a08097963e430db456e0344f3d66161b
nshupe <nickshupe10@outlook.com>
2024-04-29 16:54:11 -0500

    cookie storage works, and deleted _Host.cshtml due to it not being used in framework 8.0

--- BlazorApp/wwwroot/cookieFunctions.js
+++ BlazorApp/wwwroot/cookieFunctions.js
@@ -2,7 +2,7 @@ window.setCookie = (name, value) => {
     document.cookie = `${name}=${value}; path=/;`;
 };
 
-window.document.getCookie = function(name) {
+window.getCookie = function(name) {
     var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
     if (match) {
         return match[2];
