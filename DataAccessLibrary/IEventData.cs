6f3438dd948baa75bc5b5a993b2989c8cf7dbdee
nshupe <nickshupe10@outlook.com>
2024-05-01 18:39:12 -0500

    event.razor now saves it to the database, and test loads events by pulling the profile Id

--- DataAccessLibrary/IEventData.cs
+++ DataAccessLibrary/IEventData.cs
@@ -1,13 +1,16 @@
 using DataAccessLibrary.Models;
+using NUnit.Framework.Internal.Execution;
 
 namespace DataAccessLibrary;
 
 public interface IEventData
 {
     Task<int> AddEvent(DateTime eventDate, int hostProfileId);
-   /* public  Task<List<EventModel>> GetEvents();
-    public Task<int> InsertEvent(EventModel eventModel);
-    Task InsertMatch(int eventId, int wrestler1Id, int wrestler2Id, string matchResult);
-    Task<List<MatchModel>> GetMatchesForEvent(int eventId);*/
-    
+
+    Task<List<EventModel>> GetEventsByHostId(int hostId);
+    /* public  Task<List<EventModel>> GetEvents();
+     public Task<int> InsertEvent(EventModel eventModel);
+     Task InsertMatch(int eventId, int wrestler1Id, int wrestler2Id, string matchResult);
+     Task<List<MatchModel>> GetMatchesForEvent(int eventId);*/
+
 }
